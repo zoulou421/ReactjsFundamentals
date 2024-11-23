f556c42aecf743f1daf62967896fac780757c53c
Bonevy BEBY <bonevybeby@gmail.com>
2024-11-23 17:52:15 +0100

    added:compute method

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="m-5">
        <Counter title="Counter 1" value={1} image="images/profile1.png" />
+       <Counter title="Counter 2" value={2} image="images/profile2.png" />
+       <Counter title="Counter 3" value={3} image="images/profile3.png" />
     </div>
   );
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -4,7 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Counter extends Component {
   constructor(props){
     super(props);
+    this.state={
+      counter:1,
+      list:[0]
+    }
   }
+  compute=(op)=>{
+     let cpt=this.state.counter+1;
+     this.setState({
+      counter:cpt,
+      list:new Array(cpt).fill(0)
+     });
+  }
+  
   render() {
     return (
       <div class="card">
@@ -15,6 +27,11 @@ class Counter extends Component {
           </div>
         <div className="card-body">
          <img src={this.props.image} width={100} />
+        </div>
+
+        <div className="ml-auto">
+        <button className="btn btn-primary m-2" onClick={()=>this.compute('+')}>+</button>
+        <button className="btn btn-primary m-2" onClick={()=>this.compute('-')}>-</button>
         </div>
         <div className="card-footer">Footer</div>
       </div>
